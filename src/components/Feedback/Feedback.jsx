c08b624b1973b3427403efdf00a2208058fb0bd9
Alex <103459735+torn80beta@users.noreply.github.com>
2023-04-11 20:28:28 +0300

    step 2

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import FeedbackButtons from 'components/Feedback/FeedbackButtons';
 import Statistics from 'components/Feedback/Statistics';
 
@@ -11,20 +11,37 @@ class Feedback extends React.Component {
     good: this.props.initialValue,
     neutral: this.props.initialValue,
     bad: this.props.initialValue,
+    total: this.props.initialValue,
+    positive: this.props.initialValue,
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    const totalFeedbacks = good + neutral + bad;
+    this.setState({ total: totalFeedbacks });
   };
 
   handleIncrementGoodVal = () => {
-    this.setState(prevState => ({
-      good: prevState.good + 1,
-    }));
+    this.setState(
+      prevState => ({
+        good: prevState.good + 1,
+      }),
+      this.countTotalFeedback
+    );
   };
 
   handleIncrementNeutralValue = () => {
-    this.setState(prevState => ({ neutral: prevState.neutral + 1 }));
+    this.setState(
+      prevState => ({ neutral: prevState.neutral + 1 }),
+      this.countTotalFeedback
+    );
   };
 
   handleIncrementBadValue = () => {
-    this.setState(prevState => ({ bad: prevState.bad + 1 }));
+    this.setState(
+      prevState => ({ bad: prevState.bad + 1 }),
+      this.countTotalFeedback
+    );
   };
 
   render() {
@@ -36,6 +53,7 @@ class Feedback extends React.Component {
           incrementGoodValue={this.handleIncrementGoodVal}
           incrementNeutralValue={this.handleIncrementNeutralValue}
           incrementBadValue={this.handleIncrementBadValue}
+          //   incrementTotalValue={this.countTotalFeedback}
         />
 
         <Statistics state={this.state} />
--- src/components/Feedback/Statistics.jsx
+++ src/components/Feedback/Statistics.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const Statistics = ({ state: { good, neutral, bad } }) => {
+const Statistics = ({ state: { good, neutral, bad, total } }) => {
   return (
     <>
       <h2>Statistics</h2>
       <span className="Statistics__item">Good: {good}</span>
       <span className="Statistics__item">Neutral: {neutral}</span>
       <span className="Statistics__item">Bad: {bad}</span>
+      <span className="Statistics__item">Total: {total}</span>
     </>
   );
 };
